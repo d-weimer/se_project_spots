40ab6ca205c3045bf4b72448abb8602fd28ebbb5
d-weimer <dweimer19@gmail.com>
2025-12-13 02:44:00 +0900

    Spot Stage 4 added JS

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostButton = document.querySelector(".profile__add-button");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
